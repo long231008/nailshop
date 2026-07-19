4af8ba252d8841f342fedff0305f77ecc72754a0
long231008 <forreverloved5@gmail.com>
2026-07-19 12:30:57 +0100

    implement auth register pending flow

--- app/shared/infrastructure/database/session.py
+++ app/shared/infrastructure/database/session.py
@@ -1,19 +1,16 @@
-# app/shared/infrastructure/database/session.py
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from app.shared.infrastructure.config.settings import settings
 
 
-# Tạo kết nối tới PostgreSQL
 engine = create_engine(
     settings.DATABASE_URL,
+    echo=True,
     pool_pre_ping=True
 )
 
 
-# Tạo database session factory
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
@@ -21,12 +18,10 @@ SessionLocal = sessionmaker(
 )
 
 
-# Dependency cho FastAPI
 def get_db():
     db = SessionLocal()
 
     try:
         yield db
-
     finally:
         db.close()
